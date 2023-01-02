4f18530bd9ee119356151d64b82ce46e7fff8dfe
SArshiaK <arshia9314@gmail.com>
2023-01-02 22:32:44 +0330

    session added for GET /authors

--- server/src/routes/authors/authors.router.js
+++ server/src/routes/authors/authors.router.js
@@ -4,7 +4,17 @@ authorsRouter = express.Router();
 
 const authorsController = require('./authors.controller');
 
-authorsRouter.get('/authors', authorsController.httpGetAllAuthors);
+function checkLoggedIn(req, res, next){
+    console.log('Current user is: ', req.user);
+    const loggedIn = req.isAuthenticated() && req.user;
+    if(!loggedIn){
+        return res.status(400).json({error: "Please log in first"});
+    }
+    next();
+}
+
+authorsRouter.get('/authors', checkLoggedIn, authorsController.httpGetAllAuthors); // It has Session log in check
+
 authorsRouter.post('/authors', authorsController.httpCreateAuthor);
 authorsRouter.delete('/authors/:id', authorsController.httpDeleteAuthor);
 authorsRouter.patch('/authors/:id', authorsController.httpUpdateAuthor);
--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,10 @@
 const http = require('http');
 
 const express = require('express');
+const path = require('path');
+const passport = require('passport');
+const { Strategy } = require('passport-google-oauth20');
+const cookieSession = require('cookie-session')
 
 require('dotenv').config();
 
@@ -8,13 +12,56 @@ const cors = require("cors");
 
 const PORT = process.env.PORT || 8000;
 
+const config = {
+    CLIENT_ID: process.env.CLIENT_ID,
+    CLIENT_SECRET: process.env.CLIENT_SECRET,
+    COOKIE_KEY_1: process.env.COOKIE_KEY_1,
+    COOKIE_KEY_2: process.env.COOKIE_KEY_2
+}
+
 const { mongoConnect } = require('./services/mongo');
 
 const authorsRouter = require('./routes/authors/authors.router');
 const postRouter = require('./routes/posts/posts.router');
 const commentRouter = require('./routes/comments/comments.router');
 
+const AUTH_OPTIONS = {
+    callbackURL: '/auth/google/callback',
+    clientID: config.CLIENT_ID,
+    clientSecret: config.CLIENT_SECRET
+}
+
+function verufyCallback(accessToken, refreshToken, profile, done){
+    console.log('Google Profile', profile);
+    // const authorid = profile['id'];
+    // const gmail = profile['_json']['email'];
+    // console.log(authorid + ' ' + gmail);
+    console.log(accessToken);
+    done(null, profile);
+}
+
+passport.use(new Strategy(AUTH_OPTIONS, verufyCallback));
+// Save Session to cookie
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+// Read session from the cookie
+passport.deserializeUser((id, done) => {
+    // User.findById(id).then(user => {
+    //     done(null, user);
+    // })
+    done(null, id);
+});
+
 const app = express();
+
+app.use(cookieSession({
+    name: 'session',
+    maxAge: 1000 * 60 * 60 * 24,// ms
+    keys: [ config.COOKIE_KEY_1, config.COOKIE_KEY_2 ]
+}))
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(cors({
     origin: "http://localhost:8000"
@@ -22,6 +69,29 @@ app.use(cors({
 );
 
 app.use(express.json());
+
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['email',]
+}));
+app.get('/auth/google/callback', passport.authenticate('google', {
+    failureRedirect: '/failure',
+    successRedirect: '/',
+    session: true,
+}), (req, res) => {
+    console.log('Google called us back');
+});
+app.get('/auth/logout', (req, res) => {
+    req.logOut();
+    return res.redirect('/');
+});
+
+app.get('/failure', (req, res) => {
+    return res.send('Failed to log in');
+})
+
+app.get('/',function(req,res) {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.use('/', authorsRouter);
 app.use('/', postRouter);
